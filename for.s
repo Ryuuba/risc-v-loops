c04ee716fd898e40ef70eec0da1acb18baf203a2
A. G. Medrano-Chavez <ryuuba@gmail.com>
2022-08-17 18:00:45 -0500

    Update conditional-branch labels

--- for.s
+++ for.s
@@ -2,6 +2,6 @@
         addi s1, zero, 0
         addi s2, zero, 0
         j    for1
-LF1:    addi s1, s1, 1
-for1:   blt  s1, s2, LF1
+L1:     addi s1, s1, 1
+for1:   blt  s1, s2, L1
         nop
--- while.s
+++ while.s
@@ -2,6 +2,6 @@
         addi s1, zero, 0
         addi s2, zero, 0
         j    wh1
-LW1:    addi s1, s1, 1
-wh1:    blt  s1, s2, LW1
+L1:     addi s1, s1, 1
+wh1:    blt  s1, s2, L1
         nop
